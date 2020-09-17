1320426d0a785b82bdf964783d4372e9e15a9fad
Chris Farber <chris@chrisfarber.net>
2020-09-16 21:25:53 -0400

    sketch out multimethod for content

---
+++ src/saison/content.clj
@@ -0,0 +1,28 @@
+(ns saison.content
+  (:require [clojure.java.io :as io]))
+
+(defmulti content->input-stream
+  "Coerce data to an input stream.
+
+  Should be used with with-open to ensure that the stream is closed."
+  type)
+
+(derive java.io.File ::streamable)
+(derive java.io.InputStream ::streamable)
+
+(defmethod content->input-stream ::streamable
+  [value]
+  (io/input-stream value))
+
+(defmethod content->input-stream java.lang.String
+  [string]
+  (-> string
+      .getBytes
+      java.io.ByteArrayInputStream.))
+
+(comment
+  (data-type "hi")
+  (with-open [data (content->input-stream "it works?")]
+    (slurp data))
+  )
+
