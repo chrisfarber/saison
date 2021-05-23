f9e31fda928522c228850b461efd1d8f57b53ca9
Chris Farber <chris@chrisfarber.net>
2021-05-23 14:51:32 -0400

    Update deps, tools, test runner

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,2 +1,3 @@
-((nil . ((cider-clojure-cli-global-options . "-A:test")
-	 )))
+((nil . ((cider-clojure-cli-global-options . "-A:test:dev")
+	 (inf-clojure-custom-startup . "clojure -A:test:dev")
+	 (inf-clojure-custom-repl-type . clojure))))
