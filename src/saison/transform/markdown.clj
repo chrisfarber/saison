838a47d311c9bd1d6c5200a85b46f75f7314560a
Chris Farber <chris@chrisfarber.net>
2021-06-18 10:47:23 -0400

    wip: spike out usage of flexmark

--- src/saison/transform/markdown.clj
+++ src/saison/transform/markdown.clj
@@ -4,19 +4,31 @@
             [saison.content :as content]
             [saison.path :as path]
             [saison.source :as source]
-            [saison.util :as util]))
+            [saison.util :as util]
+            [clojure.java.io :as io])
+  (:import [com.vladsch.flexmark.html HtmlRenderer]
+           [com.vladsch.flexmark.parser Parser]
+           [com.vladsch.flexmark.util.data MutableDataSet]
+           [com.vladsch.flexmark.ext.yaml.front.matter YamlFrontMatterExtension]))
 
 (defn ext-to-html [path]
   (util/set-path-extension (path/pathname path) "html"))
 
-(defn parse-markdown [path]
+(defn parse-markdown [stream]
+  (let [options (doto (MutableDataSet.)
+                  (.set Parser/EXTENSIONS [(YamlFrontMatterExtension/create)]))
+        parser (.build (Parser/builder options))
+        renderer (.build (HtmlRenderer/builder options))
+        document (with-open [rdr (io/reader stream)]
+                   (.parseReader parser rdr))]
+    {:html (.render renderer document)
+     :metadata {}}))
+
+(defn parse-markdown-from-path [path]
   (let [existing-meta (path/metadata path)
-        markdown-str (-> path path/content content/string)
-        {:keys [metadata html]} (md-to-html-string-with-meta
-                                 markdown-str
-                                 :parse-meta? true
-                                 :footnotes? true
-                                 :reference-links? true)
+        markdown-content (path/content path)
+        markdown-stream (content/input-stream markdown-content)
+        {:keys [metadata html]} (parse-markdown markdown-stream)
         ;; md-to-meta gives us a map of keywords to arrays of strings.
         ;; (because you could supply the same header twice)
         ;; here I arbitrarily take the first value.
@@ -36,7 +48,7 @@
    :name "markdown"
    :where markdown?
    :pathname ext-to-html
-   :metadata-and-content parse-markdown))
+   :metadata-and-content parse-markdown-from-path))
 
 (defn markdown
   "A source step that will parse any markdown path into HTML."
