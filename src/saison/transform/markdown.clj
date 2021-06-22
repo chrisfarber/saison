fa6750843c8960bb588280660e9ba8dc58def0c3
Chris Farber <chris@chrisfarber.net>
2021-06-21 20:37:49 -0400

    allow for customization of flexmark parser

--- src/saison/transform/markdown.clj
+++ src/saison/transform/markdown.clj
@@ -12,41 +12,48 @@
 (defn ext-to-html [path]
   (util/set-path-extension (path/pathname path) "html"))
 
-(defn parse-markdown [str]
-  (let [options (doto (MutableDataSet.)
-                  (.set TablesExtension/COLUMN_SPANS false)
-                  (.set TablesExtension/APPEND_MISSING_COLUMNS true)
-                  (.set TablesExtension/DISCARD_EXTRA_COLUMNS true)
-                  (.set TablesExtension/HEADER_SEPARATOR_COLUMN_MATCH true)
-                  (.set Parser/EXTENSIONS [(TablesExtension/create)]))
+(defn build-default-parser-options []
+  (doto (MutableDataSet.)
+    (.set TablesExtension/COLUMN_SPANS false)
+    (.set TablesExtension/APPEND_MISSING_COLUMNS true)
+    (.set TablesExtension/DISCARD_EXTRA_COLUMNS true)
+    (.set TablesExtension/HEADER_SEPARATOR_COLUMN_MATCH true)
+    (.set Parser/EXTENSIONS [(TablesExtension/create)])))
+
+(defn markdown-parser [build-parser-options]
+  (let [options (build-parser-options)
         parser (.build (Parser/builder options))
-        renderer (.build (HtmlRenderer/builder options))
-        document (.parse parser str)]
-    (.render renderer document)))
+        renderer (.build (HtmlRenderer/builder options))]
+    (fn [str]
+      (let [document (.parse parser str)]
+        (.render renderer document)))))
 
 (defn update-mime [path]
   (assoc (path/metadata path)
          :mime-type "text/html"))
 
-(defn parse-markdown-from-path [path]
-  (let [markdown-content (path/content path)
-        markdown-string (content/string markdown-content)
-        html (parse-markdown markdown-string)]
-    html))
+(defn parse-markdown-from-path
+  [build-parser-options]
+  (let [parser (markdown-parser build-parser-options)]
+    (fn [path]
+      (let [markdown-content (path/content path)
+            markdown-string (content/string markdown-content)]
+        (parser markdown-string)))))
 
 (defn markdown?
   [path]
   (#{"md" "markdown"} (util/path-extension (path/pathname path))))
 
-(defn markdown-transformer []
+(defn markdown-transformer [build-parser-options]
   (path/transformer
    :name "markdown"
    :where markdown?
    :pathname ext-to-html
    :metadata update-mime
-   :content parse-markdown-from-path))
+   :content (parse-markdown-from-path build-parser-options)))
 
 (defn markdown
   "A source step that will parse any markdown path into HTML."
-  []
-  (source/transform-paths (markdown-transformer)))
+  [& {:keys [build-parser-options]
+      :or {build-parser-options build-default-parser-options}}]
+  (source/transform-paths (markdown-transformer build-parser-options)))
